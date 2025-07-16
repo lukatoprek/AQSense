e938f26fbe4f9fd1760ee5706f5952d83d418f2c
lukatoprek <luka.toprek@gmail.com>
2025-07-16 10:18:22 +0200

    Fix sdk configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
-// root/build.gradle.kts
 plugins {
     id("com.android.application") version "8.8.0-rc02" apply false
     id("com.android.library") version "8.8.0-rc02" apply false
-    kotlin("multiplatform") version "2.1.0" apply false
+    kotlin("multiplatform") version "2.0.0" apply false
     id("org.jetbrains.compose") version "1.7.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    kotlin("plugin.compose") version "2.1.0" apply false
+    kotlin("plugin.compose") version "2.0.0" apply false
 }
